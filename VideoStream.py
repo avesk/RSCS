0feac70d312dbcf00451348caad23e48d1b6dda6
avesk <averykushner@gmail.com>
2020-05-24 18:23:25 -0700

    adds flask app

--- VideoStream.py
+++ VideoStream.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, Response
-from camera import Camera
+app = Flask(__name__)
+
+import cv2 as cv
+
+if importlib.find_loader("picamera"):
+    from picamera import PiCamera
+	Camera = cv.VideoCapture(PiCamera())
+else:
+	Camera = cv.VideoCapture(0)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
+def gen(camera):
+    while True:
+        ret, frame = camera.read()
+		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
