ce94e9c3f53f1906badc5fd2bfc16f00f8fa4793
avesk <averykushner@gmail.com>
2020-05-31 12:59:13 -0700

    capturing at 30fps at 480x640

--- VideoCapture.py
+++ VideoCapture.py
@@ -5,20 +5,30 @@ class VideoCapture(cv.VideoCapture):
     to simplify the interface for this Applications
     VideoCapturing requirements
     '''
-    def __init__(self, fps=60):
+    def __init__(self, fps=30, res=(1920,1080)):
         super().__init__(0)
-        self.set_frame_rate(fps)
+        self.set_resolution(res)
 
     def set_frame_rate(self, fps):
         '''Sets the framerate of capture object
 
+        @todo investigate if this has any affect on PiCam
+        streaming.
+
         Params:
             fps (float) : framerate in frames per second
         '''
-        self.set(
-            int(self.get(cv.CAP_PROP_FPS)), 
-            fps
-        )
+        self.set(cv.CAP_PROP_FPS, fps)
+    
+    def set_resolution(self, resolution):
+        '''Sets the frameresolution rate of capture object
+
+        Params:
+            resolution (tuple(int)) : WxH resolution
+        '''
+        w, h = resolution
+        self.set(cv.CAP_PROP_FRAME_WIDTH, w)
+        self.set(cv.CAP_PROP_FRAME_HEIGHT, h)
     
     def capture(self):
         '''Captures next frame, and returns a .JPG encoded 
@@ -30,3 +40,4 @@ class VideoCapture(cv.VideoCapture):
         _, jpg = cv.imencode('.jpg', frame)
         return jpg
     
+
